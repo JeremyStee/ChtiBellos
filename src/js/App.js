8bdf6ca4b540ae71bf078e4d81a7063720a3df24
Jérémy Steenkiste <jeremy.steenkiste.pro@gmail.com>
2018-10-24 12:34:19 +0200

    Projet opérationnel

---
+++ src/js/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+//SCSS
+import '../scss/App.scss';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Bonjour</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
